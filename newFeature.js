b034ad53a29ea8a026a49550ed111d51a7487738
Ulshin Max <max.ulshin.max@istat.com.ua>
2020-10-03 14:28:59 +0300

    Fix feature 1
    Very impressive fix of that feature.
    It will make system meltdown if i do not adding new lines of code for
    this feature.
    But now all is okay.

--- newFeature.js
+++ newFeature.js
@@ -1,9 +1,13 @@
 function feature1(){
     console.log('ouuuu eeee')
 }
+const mainFixes = () => {
+    console.log('ya ya')
+}
 
 const main  = () => {
     feature1()
+    mainFixes()
 }
 
 main();
